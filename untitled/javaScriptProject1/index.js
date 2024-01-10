b95e659167592a72a730b02dcf5e31f3f18d3c8b
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2024-01-10 16:55:56 +0100

    worked on store page

--- untitled/javaScriptProject1/index.js
+++ untitled/javaScriptProject1/index.js
@@ -1,5 +1,5 @@
 // let students = 30
-
+//
 // students = students + 1;
 // students = students - 1;
 // students = students * 2;
--- untitled/javaScriptProject3/index.js
+++ untitled/javaScriptProject3/index.js
@@ -2,7 +2,7 @@
 // let x = 3;
 // let y = 2;
 // let z = 1;
-
+//
 // z = Math.round(x);
 // z = Math.floor(x);
 // z = Math.ceil(x);
