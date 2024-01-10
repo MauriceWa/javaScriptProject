19c3b74532a51e1bba5dbf2b0935020c491498f5
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2024-01-10 19:25:45 +0100

    worked on js

---
+++ untitled/javaScriptProject8/index.js
@@ -0,0 +1,29 @@
+// const fullName = "Maurice Wa";
+//
+// let firstName = fullName.slice(0, 7);
+// let lastName = fullName.slice(8);
+//
+// let firstChar = fullName.slice(0,1);
+// let firstChar = fullName.slice(-1);
+// console.log(firstChar);
+// console.log(lastChar);
+
+
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") +1);
+
+// console.log(firstName);
+// console.log(lastName);
+
+
+const email = "murillowaaijer2@outlook.com";
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+
+console.log(username)
+console.log(extension)
+
+
+
+
