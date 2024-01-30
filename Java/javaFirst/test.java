7b596a571b40cb3512f2d50362bc90726ea8fb1b
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2024-01-30 22:32:00 +0100

    started java work

---
+++ Java/javaFirst/test.java
@@ -0,0 +1,8 @@
+public class testJava {
+
+    public static void main(String[] args) {
+
+        String name = 'Susan';
+        System.out.println(name.ToUpperCase());
+    }
+}
---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        String name = 'Susan';
+        System.out.println(name.toUpperCase());
+
+    }
+}
